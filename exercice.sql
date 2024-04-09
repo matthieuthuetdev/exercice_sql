153e60575f3dee1e9543dc1e21b41ea8ad1fc8c8
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-04-09 08:44:42 +0200

    ajout des requete 1 2 3 et 4

--- exercice.sql
+++ exercice.sql
@@ -1,10 +1,46 @@
-v/* 1. Sélectionner l'identifiant, le nom de tous les clients dont le numéro de téléphone commence par '04' */
-SELECT client_nom client_ref FROM clients WHERE client_telephone LIKE "04%";
+v
+/* 1. Sélectionner l'identifiant, le nom de tous les clients dont le numéro de téléphone commence par '04' */
+SELECT
+    client_nom client_ref
+FROM
+    clients
+WHERE
+    client_telephone LIKE "04%";
+
 /* 2. Sélectionner l'identifiant, le nom et le type de tous les clients qui sont des particuliers */
+SELECT
+    clients.client_ref,
+    clients.client_nom,
+    clients.type_client_id,
+    type_clients.type_client_libelle
+FROM
+    clients
+    INNER JOIN type_clients ON clients.type_client_id = type_clients.type_client_id
+WHERE
+    type_clients.type_client_libelle = 'particulier';
+
 /* 3. Sélectionner l'identifiant, le nom et le type de tous les clients qui ne sont pas des particuliers */
+SELECT
+    clients.client_ref,
+    clients.client_nom,
+    clients.type_client_id,
+    type_clients.type_client_libelle
+FROM
+    clients
+    INNER JOIN type_clients ON clients.type_client_id = type_clients.type_client_id
+WHERE
+    type_clients.type_client_libelle not in ('particulier');
+
 /* 4. Sélectionner les projets qui ont été livrés en retard */
+SELECT
+    *
+from
+    projets
+WHERE
+    projet_date_fin_prevue < projet_date_fin_effective;
+
 /* 5. Sélectionner la date de dépôt, la date de fin prévue, les superficies, le prix de tous les projets 
-    avec le nom du client et le nom de l'architecte associés au projet */
+ avec le nom du client et le nom de l'architecte associés au projet */
 /* 6. Sélectionner tous les projets (dates, superficies, prix) avec le nombre d'intervenants autres que le client et l'architecte */
 /* 7. Sélectionner les types de projets avec, pour chacun d'entre eux, le nombre de projets associés et le prix moyen pratiqué */
 /* 8. Sélectionner les types de travaux avec, pour chacun d'entre eux, la superficie du projet la pls grande */
